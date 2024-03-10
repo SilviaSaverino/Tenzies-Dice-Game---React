579e2831a3bb56d2697c38c53aafb03aba3c3f78
Silvia Saverino <silviasave89@gmail.com>
2024-03-10 21:52:30 +0000

    add createNewDiceSet function to stay DRY, and add functionality to  let rollDice function to skip the selected dice when rolling a new set

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,42 +1,49 @@
 import { useState } from 'react'
-import {nanoid} from 'nanoid'
+import { nanoid } from 'nanoid'
 import './App.css'
 import Die from "./assets/components/Die"
 
 function App() {
 
   const [newDice, setNewDice] = useState(getNewDice())
-  
-  function getNewDice(){
+
+function createNewDiceSet(){
+  return {
+    value: Math.ceil(Math.random() * 6),
+    isHeld: false,
+    id: nanoid()
+  }
+}
+
+  function getNewDice() {
     const newDiceSet = []
-    for(let i = 0;i < 10; i++){
-      newDiceSet.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid()
-      })
-    }
+    for (let i = 0; i < 10; i++) {
+      newDiceSet.push(createNewDiceSet())
+      }
     return newDiceSet
   }
-  
-  function rollDice(){
-    setNewDice(getNewDice())
-  }
 
-  function selectDieOnHold(id){
-   setNewDice(oldDice => oldDice.map(selectedDie => {
-    return selectedDie.id === id ? {...selectedDie, isHeld: !selectedDie.isHeld} : selectedDie
-   }))
+  function rollDice() {
+    setNewDice(oldDice => oldDice.map(dieToBeSkipped => {
+      return dieToBeSkipped.isHeld === true ? dieToBeSkipped : createNewDiceSet()
+  }))
+    }
+
+
+  function selectDieOnHold(id) {
+    setNewDice(oldDice => oldDice.map(selectedDie => {
+      return selectedDie.id === id ? { ...selectedDie, isHeld: !selectedDie.isHeld } : selectedDie
+    }))
   }
 
   const diceElements = newDice.map(singleDie => (
-    <Die 
-      value={singleDie.value} 
-      key={singleDie.id} 
+    <Die
+      value={singleDie.value}
+      key={singleDie.id}
       isHeld={singleDie.isHeld}
       selectDieOnHold={() => selectDieOnHold(singleDie.id)}
     />
-    ))
+  ))
   return (
     <main>
       <div className='die-container'>
