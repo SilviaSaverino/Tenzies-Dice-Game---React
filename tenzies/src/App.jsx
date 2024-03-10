5c91fe7f7f8138368ba0224bd1d721ee3dbd2583
Silvia Saverino <silviasave89@gmail.com>
2024-03-10 19:38:45 +0000

    create state and map over it to get dice values of Die component

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -3,29 +3,24 @@ import './App.css'
 import Die from "./assets/components/Die"
 
 function App() {
-  
-function getNewDice(){
-  const newDiceSet = []
-  for(let i = 0;i < 10; i++){
-    newDiceSet.push(Math.ceil(Math.random() * 6))
-  }
-  return newDiceSet
-}
-console.log(getNewDice())
 
+  const [newDice, setNewDice] = useState(getNewDice())
+  
+  function getNewDice(){
+    const newDiceSet = []
+    for(let i = 0;i < 10; i++){
+      newDiceSet.push(Math.ceil(Math.random() * 6))
+    }
+    return newDiceSet
+  }
+  console.log(getNewDice())
+
+  const diceElements = newDice.map(singleDie => <Die value={singleDie} />)
   return (
     <main>
       <div className='die-container'>
-        <Die value="1"/>
-        <Die />
-        <Die />
-        <Die />
-        <Die />
-        <Die />
-        <Die />
-        <Die />
-        <Die />
-        <Die />
+        {diceElements}
+        
       </div>
     </main>
   )
